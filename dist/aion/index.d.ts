97ca6db82b31ced7a1c72ddb936768342e5cc7ca
Arjit Khullar <arjitkhullar@gmail.com>
2018-11-17 20:09:25 -0500

    updated build

--- dist/aion/index.d.ts
+++ dist/aion/index.d.ts
@@ -1,26 +1,17 @@
-import { AbiDefinition } from 'ethereum-types';
-import Web3 from 'aion-web3-core';
 interface Deploy {
-    deployedAddress: string;
-    abi: AbiDefinition[];
     code: string;
     mainAccount: string;
     gas: number;
     gasPrice: number;
-    contractArguments: string;
+    contractArguments?: string;
 }
 export default class Aion {
-    web3: Web3;
-    constructor(providerUrl: string);
+    nodeAddress: string;
+    constructor(nodeAddress: string);
     getAccounts: () => Promise<string[]>;
-    getBalance: (address: string) => Promise<string | import("bignumber.js").BigNumber>;
-    unlock: (address: string, password: string) => boolean;
-    deploy: ({ deployedAddress, abi, code, mainAccount, gas, gasPrice, contractArguments }: Deploy) => Promise<{
-        receipt: undefined;
-        txHash: undefined;
-        confirmation: undefined;
-        newContractInstance: import("../typings/aion-web3-core/types").Contract;
-    }>;
-    estimateGas: ({ deployedAddress, abi, code, mainAccount, gas, contractArguments }: Deploy) => Promise<number>;
+    getBalance: (address: string) => Promise<number>;
+    unlock: (address: string, password: string) => Promise<boolean>;
+    deploy: ({ code, mainAccount, gas, gasPrice, contractArguments }: Deploy) => Promise<void>;
+    estimateGas: ({ code, mainAccount, gas, gasPrice }: Deploy) => Promise<any>;
 }
 export {};
--- dist/aion/index.js
+++ dist/aion/index.js
@@ -7,128 +7,78 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var aion_web3_core_1 = __importDefault(require("aion-web3-core"));
-var Aion = /** @class */ (function () {
-    function Aion(providerUrl) {
-        var _this = this;
-        this.getAccounts = function () { return __awaiter(_this, void 0, void 0, function () { return __generator(this, function (_a) {
-            return [2 /*return*/, this.web3.eth.getAccounts()];
-        }); }); };
-        this.getBalance = function (address) { return __awaiter(_this, void 0, void 0, function () { var _a, _b; return __generator(this, function (_c) {
-            switch (_c.label) {
-                case 0:
-                    _b = (_a = this.web3.utils).fromNAmp;
-                    return [4 /*yield*/, this.web3.eth.getBalance(address)];
-                case 1: return [2 /*return*/, _b.apply(_a, [_c.sent(), 'nAmp'])];
-            }
-        }); }); };
-        this.unlock = function (address, password) {
-            return _this.web3.eth.personal.unlockAccount(address, password);
-        };
-        this.deploy = function (_a) {
-            var deployedAddress = _a.deployedAddress, abi = _a.abi, code = _a.code, mainAccount = _a.mainAccount, gas = _a.gas, gasPrice = _a.gasPrice, contractArguments = _a.contractArguments;
-            return __awaiter(_this, void 0, void 0, function () {
-                var receipt, txHash, confirmation, contract, newContractInstance;
-                return __generator(this, function (_b) {
-                    switch (_b.label) {
-                        case 0:
-                            contract = new this.web3.eth.Contract(abi, deployedAddress, {
-                                from: mainAccount,
-                                data: code,
-                                gas: gas
-                            });
-                            return [4 /*yield*/, contract
-                                    .deploy({
-                                    data: code,
-                                    arguments: contractArguments.split(',').slice()
-                                })
-                                    .send({
-                                    from: mainAccount,
-                                    gas: gas,
-                                    gasPrice: gasPrice
-                                })
-                                    .on('receipt', function (_receipt) {
-                                    receipt = _receipt;
-                                })
-                                    .on('error', function (error) {
-                                    throw error;
-                                })
-                                    .on('transactionHash', function (_txHash) {
-                                    txHash = _txHash;
-                                })
-                                    .on('confirmation', function (confNumber, confReceipt) {
-                                    confirmation = { confNumber: confNumber, confReceipt: confReceipt };
-                                })];
-                        case 1:
-                            newContractInstance = _b.sent();
-                            return [2 /*return*/, {
-                                    receipt: receipt,
-                                    txHash: txHash,
-                                    confirmation: confirmation,
-                                    newContractInstance: newContractInstance
-                                }];
-                    }
-                });
+const axios_1 = __importDefault(require("axios"));
+class Aion {
+    constructor(nodeAddress) {
+        this.getAccounts = () => __awaiter(this, void 0, void 0, function* () {
+            const { data: { result } } = yield axios_1.default.post(this.nodeAddress, {
+                jsonrpc: '2.0',
+                method: 'eth_accounts',
+                params: [],
+                id: 1
             });
-        };
-        this.estimateGas = function (_a) {
-            var deployedAddress = _a.deployedAddress, abi = _a.abi, code = _a.code, mainAccount = _a.mainAccount, gas = _a.gas, contractArguments = _a.contractArguments;
-            return __awaiter(_this, void 0, void 0, function () {
-                var contract, estimatedGas;
-                return __generator(this, function (_b) {
-                    switch (_b.label) {
-                        case 0:
-                            contract = new this.web3.eth.Contract(abi, deployedAddress, {
-                                from: mainAccount,
-                                data: code,
-                                gas: gas
-                            });
-                            return [4 /*yield*/, contract
-                                    .deploy({
-                                    data: code,
-                                    arguments: contractArguments.split(',').slice()
-                                })
-                                    .estimateGas()];
-                        case 1:
-                            estimatedGas = _b.sent();
-                            return [2 /*return*/, estimatedGas];
-                    }
-                });
+            return result;
+        });
+        this.getBalance = (address) => __awaiter(this, void 0, void 0, function* () {
+            const { data: { result } } = yield axios_1.default.post(this.nodeAddress, {
+                jsonrpc: '2.0',
+                method: 'eth_getBalance',
+                params: [address, 'latest'],
+                id: 1
             });
-        };
-        this.web3 = new aion_web3_core_1.default(new aion_web3_core_1.default.providers.HttpProvider(providerUrl));
+            return +result;
+        });
+        this.unlock = (address, password) => __awaiter(this, void 0, void 0, function* () {
+            const { data: { result } } = yield axios_1.default.post(this.nodeAddress, {
+                jsonrpc: '2.0',
+                method: 'personal_unlockAccount',
+                params: [address, password],
+                id: 1
+            });
+            return result;
+        });
+        this.deploy = ({ code, mainAccount, gas, gasPrice, contractArguments }) => __awaiter(this, void 0, void 0, function* () {
+            const { data: { result: txHash } } = yield axios_1.default.post(this.nodeAddress, {
+                jsonrpc: '2.0',
+                method: 'eth_sendTransaction',
+                params: [
+                    {
+                        from: mainAccount,
+                        data: code,
+                        gas,
+                        gasPrice
+                    }
+                ],
+                id: 1
+            });
+            const { data: { result: txReceipt } } = yield axios_1.default.post(this.nodeAddress, {
+                jsonrpc: '2.0',
+                method: 'eth_getTransactionReceipt',
+                params: [txHash],
+                id: 1
+            });
+        });
+        this.estimateGas = ({ code, mainAccount, gas, gasPrice }) => __awaiter(this, void 0, void 0, function* () {
+            const { data: { result } } = yield axios_1.default.post(this.nodeAddress, {
+                jsonrpc: '2.0',
+                method: 'eth_estimateGas',
+                params: [
+                    {
+                        from: mainAccount,
+                        data: code,
+                        gas,
+                        gasPrice
+                    }
+                ],
+                id: 1
+            });
+            return result;
+        });
+        this.nodeAddress = nodeAddress;
     }
-    return Aion;
-}());
+}
 exports.default = Aion;
